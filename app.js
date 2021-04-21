3de5a03547983194ca09ae985235ae794cd6211a
root <root@mail.sheetcheat.xyz>
2021-04-21 08:16:34 +0000

    J

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", require("./routes/index"));
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
