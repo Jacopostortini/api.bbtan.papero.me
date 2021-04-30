be31d144e9972371ce9dbb75c4c3e50525d3ac9d
root <root@mail.sheetcheat.xyz>
2021-04-30 13:54:47 +0000

    J

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ function getCookies(request) {
 
 router.get("/game", async (req, res)=>{
     const cookies = getCookies(req);
+    console.log("Get game from ", cookies.userId);
     const game = await Game.findOne({userId: cookies.userId}, (err, game) => {return game}).exec();
     const highestScore = await HighestScore.findOne({userId: cookies.userId}, (err, highest) => {return highest}).exec();
     if(game){
@@ -26,24 +27,42 @@ router.get("/game", async (req, res)=>{
 
 router.post("/game", async (req, res) => {
     const cookies = getCookies(req);
+    console.log("Post game from ", cookies.userId);
+    console.log("Posting game: ", req.body);
     const game = await Game.findOne({userId: cookies.userId}, (err, game) => {return game;}).exec();
+    console.log("Game found: ", game);
     const highestScore = await HighestScore.findOne({userId: cookies.userId}, (err, highest) => {return highest;}).exec();
-    if(highestScore.score < game.level){
-        HighestScore.replaceOne({userId: cookies.userId}, {
-            userId: cookies.userId,
-            score: game.level,
-            scoreDate: Date.now()
-        }, {upsert: true});
+    let newScore;
+    let newScoreDate;
+    if(highestScore){
+	if(highestScore.score < req.body.level){
+	    newScore = req.body.level;
+	    newScoreDate = Date.now();
+	} else {
+  	    newScore = highestScore.score;
+	    newScoreDate = highestScore.date;
+	}
+    } else {
+	newScore = req.body.level;
+	newScoreDate = Date.now();
     }
 
-    Game.replaceOne({userId: cookies.userId}, {
+    await HighestScore.replaceOne({userId: cookies.userId}, {
+        userId: cookies.userId,
+        score: newScore,
+        scoreDate: newScoreDate
+    }, {upsert: true});   
+
+    await Game.replaceOne({userId: cookies.userId}, {
         userId: cookies.userId,
         blocks: req.body.blocks,
         level: req.body.level,
         position: req.body.position
     }, {upsert: true});
+    console.log("Game found after replace: ", await Game.findOne({userId: cookies.userId}, (err, game) => {return game;}).exec());
     res.status(200).send("OK");
 
 });
 
 module.exports = router;
+
